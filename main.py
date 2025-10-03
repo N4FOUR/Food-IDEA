88a4864dfcba61ef2f405d915bb42dbca24d31e5
Nopphon <72090943+N4FOUR@users.noreply.github.com>
2025-10-03 18:16:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 with open('recipes.json', 'r') as file: recipes = json.load(file)
 
-st.title('Food IDEA')
+# st.title('Food IDEA')
 
 AllStyle=sorted(list({item['style'] for item in recipes if item.get('style')}))
     
-styles_input = st.multiselect('Food Style', AllStyle)
-ingrediants_input = st.text_input('ingrediants')
-# styles_input = input("Style: ")
-# ingrediants_input = input("Ingrediants: ")
+# styles_input = st.multiselect('Food Style', AllStyle)
+# ingrediants_input = st.text_input('ingrediants')
+styles_input = input("Style: ")
+ingrediants_input = input("Ingrediants: ")
 
 def load_menu_f_style(styles_input):
     return [style for style in recipes if style['style'] in styles_input]
@@ -43,14 +43,13 @@ if st.button('Get Menu'):
         # print(menu_filter_for_rdm(menu, load_ingrediant_f_filter(ingrediants_input)))
         menu_list.append(menu_filter_for_rdm(menu, load_ingrediant_f_filter(ingrediants_input)))
 
-    reccommend_list = sorted(menu_list, key=lambda x: x[-1] for x in menu_list, reverse=True)
+    reccommend_list = sorted(menu_list, key=lambda x: x[-1] if x else 0, reverse=True)
     for item in reccommend_list:
-        dish=item[0]
-        missing_ing=item[1]
-        with st.container(border=True):
-            st.write(dish['style'])
-            st.write(dish['name'])
-            st.caption(f'Match Score : {item[2]}')
-            st.caption(f'Missing Ingrediants : {missing_ing}')
-
-
+        if item:
+            dish=item[0]
+            missing_ing=item[1]
+            with st.container(border=True):
+                st.write(dish['style'])
+                st.write(dish['name'])
+                st.caption(f'Match Score : {item[2]}')
+                st.caption(f'Missing Ingrediants : {missing_ing}')
