996c1be577a554873575da6b3332b29004ad8cee
Nopphon <72090943+N4FOUR@users.noreply.github.com>
2025-10-03 23:02:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import json
 import streamlit as st
 
-with open('recipes.json', 'r') as file: recipes = json.load(file)
+# load dataset 
+with open('recipes.json', 'r') as file: recipes = json.load(file) 
 
-st.title('Food IDEA')
+st.title('Food IDEA') # title text
 
+# filter style for multiselect
 AllStyle=sorted(list({item['style'] for item in recipes if item.get('style')}))
-    
-styles_input = st.multiselect('Food Style', AllStyle)
-ingrediants_input = st.text_input('ingrediants')
 
+# user input
+styles_input = st.multiselect('อาหารของประเทศ / ไม่เลือกระบบจะดึงมาจากทุกอัน', AllStyle)
+ingrediants_input = st.text_input('วัตถุดิบที่มี เช่น แป้งสาลี, ไข่, นม, น้ำตาล, เนย')
+
+# filter style for user selected style
 def _filter_menu_style(styles_input):
     if styles_input: return [style for style in recipes if style['style'] in styles_input]
     else: return recipes
 
+# convert ingrediants input for easy use
 def _filter_user_ingrediants(ingrediants_input):
     if ingrediants_input:
         return [str(ing).lower() for ing in ingrediants_input.split(',')]
     else:
         return []
 
+# filter menu for recommend result
 def _menu_filter_for_rdm(menu, userinput):
     match_ing_list = [ing for ing in menu['ingredients'] if ing in userinput]
     if match_ing_list:return menu
     else: return None
 
+# button for Start filter
 if st.button('Enter'):
     for menu in _filter_menu_style(styles_input):
-        recdm_food = _menu_filter_for_rdm(menu, _filter_user_ingrediants(ingrediants_input))
-        if recdm_food:
-            missing_ing = [ing for ing in recdm_food["ingredients"] if ing not in _filter_user_ingrediants(ingrediants_input)]
+        recommend_menu = _menu_filter_for_rdm(menu, _filter_user_ingrediants(ingrediants_input))
+        if recommend_menu:
+            missing_ing = [ing for ing in recommend_menu["ingredients"] if ing not in _filter_user_ingrediants(ingrediants_input)]
+            # output
             with st.container(border=True):
-                st.write(f'{recdm_food["style"]} : {recdm_food["name"]}')
-                st.text(f'Category : {recdm_food['category']}')
-                st.text(f'Vegetarian : {recdm_food["vegetarian"]}')
-                st.caption(f'Ingrediants : {", ".join(recdm_food["ingredients"])}')
-                st.caption(f'Missing Ingrediants : {", ".join(missing_ing)}' if missing_ing else 'Missing Ingrediants : None')
+                st.write(f'{recommend_menu["style"]} : {recommend_menu["name"]}')
+                st.text(f'ประเภท : {recommend_menu['category']}')
+                st.text(f'มังสวิรัติ : {recommend_menu["vegetarian"]}')
+                st.caption(f'วัตถุดิบ : {", ".join(recommend_menu["ingredients"])}')
+                st.caption(f'วัตถุดิบที่ขาด : {", ".join(missing_ing)}' if missing_ing else 'วัตถุดิบที่ขาด : None')
